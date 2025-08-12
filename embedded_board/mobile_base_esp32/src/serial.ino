1f782de63e12fb2ddf5bf223dd3d4127410dc00a
Bence Magyar <bence.magyar.robotics@gmail.com>
2025-08-12 12:48:55 +0100

    keep tweaking, keep keep tweaking

--- embedded_board/mobile_base_esp32/src/serial.ino
+++ embedded_board/mobile_base_esp32/src/serial.ino
@@ -1,94 +1,67 @@
 #include <Arduino.h>
+#include <zenoh-pico.h>
 
-extern "C" {
-#include "zenoh-pico.h"
-}
-
+#define BLINK_DURATION_MS 1000
 #define MODE "client"
-#define LOCATOR "serial/20.19#baudrate=115200"
-#define KEYEXPR "demo/example/zenoh-pico-pub"
-#define VALUE "[ARDUINO]{ESP32} Publication from Zenoh-Pico via Serial!"
+// #define CONNECT "serial/10.9#baudrate=115200"
+#define CONNECT "serial/17.16#baudrate=115200"
+// #define KEYEXPR "playground/hello/*"
 
 z_owned_session_t s;
-z_owned_publisher_t pub;
-static int idx = 0;
+z_owned_subscriber_t sub;
 
 void setup() {
-  digitalWrite(RGB_BUILTIN, HIGH); // Turn the RGB LED white
-  delay(1000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0); // Red
+  delay(BLINK_DURATION_MS);
+  digitalWrite(RGB_BUILTIN, LOW); // Turn the RGB LED white
 
   Serial.begin(115200);
   while (!Serial) {
     delay(1000);
   }
-
-  digitalWrite(RGB_BUILTIN, LOW); // Turn the RGB LED off
-  delay(1000);
+  Serial.println("");
+  Serial.println("Serial port initialized!");
 
   // Initialize Zenoh Session and other parameters
   z_owned_config_t config;
   z_config_default(&config);
   zp_config_insert(z_config_loan_mut(&config), Z_CONFIG_MODE_KEY, MODE);
-  zp_config_insert(z_config_loan_mut(&config), Z_CONFIG_CONNECT_KEY, LOCATOR);
+  zp_config_insert(z_config_loan_mut(&config), Z_CONFIG_CONNECT_KEY, CONNECT);
 
   // Open Zenoh session
+  Serial.print("Opening Zenoh Session... ");
+  z_result_t res = z_open(&s, z_config_move(&config), NULL);
+  Serial.print("Let's see how that session is doing... ");
 
-  if (z_open(&s, z_config_move(&config), NULL) < 0) {
+  if (res < 0) {
+    Serial.print("Unable to open session! Code: ");
+    Serial.println(res);
     while (1) {
       neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0); // Red
-      delay(500);
-      neopixelWrite(RGB_BUILTIN, 0, 0, 0); // Red
-      delay(500);
+      delay(BLINK_DURATION_MS);
+      neopixelWrite(RGB_BUILTIN, 0, 0, 0);
+      delay(BLINK_DURATION_MS);
     }
   }
+  Serial.println("Zenoh session opened successfully");
 
   // Start read and lease tasks for zenoh-pico
+  Serial.print("Starting read and lease tasks... ");
   if (zp_start_read_task(z_session_loan_mut(&s), NULL) < 0 ||
       zp_start_lease_task(z_session_loan_mut(&s), NULL) < 0) {
+    Serial.println("Unable to start read and lease tasks");
     z_session_drop(z_session_move(&s));
     while (1) {
-      neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0); // Red
-      delay(500);
-      neopixelWrite(RGB_BUILTIN, 0, 0, 0); // Red
-      delay(500);
+      delay(1000);
     }
   }
-
-  neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0); // Red
-  delay(1000);
-
-  // Declare Zenoh publisher
-  z_view_keyexpr_t ke;
-  z_view_keyexpr_from_str_unchecked(&ke, KEYEXPR);
-  if (z_declare_publisher(z_session_loan(&s), &pub, z_view_keyexpr_loan(&ke),
-                          NULL) < 0) {
-    while (1) {
-      neopixelWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS); // Red
-      delay(500);
-      neopixelWrite(RGB_BUILTIN, 0, 0, 0); // Red
-      delay(500);
-    }
-  }
-
-  neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0); // Green
-  delay(1000);
+  Serial.println("Tasks leased successfully");
 }
 
 void loop() {
-  neopixelWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS); // Blue
-  delay(1000);
-
-  char buf[256];
-  sprintf(buf, "[%4d] %s", idx++, VALUE);
-
-  // Create payload
-  z_owned_bytes_t payload;
-  z_bytes_copy_from_str(&payload, buf);
-  if (z_publisher_put(z_publisher_loan(&pub), z_bytes_move(&payload), NULL) <
-      0) {
-    // Error while publishing data
-  }
-
-  neopixelWrite(RGB_BUILTIN, 0, 0, 0); // Off / black
-  delay(1000);
+  neopixelWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS); // Red
+  delay(BLINK_DURATION_MS);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(BLINK_DURATION_MS);
 }
