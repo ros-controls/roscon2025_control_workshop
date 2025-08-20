0be049b7bb96300895a473928ce05b14dcc43318
Bence Magyar <bence.magyar.robotics@gmail.com>
2025-08-20 15:37:55 +0100

    Update twist_publisher/include/ucdr/config.h
    Co-authored-by: Marq Rasmussen <marq.razz@gmail.com>

--- twist_publisher/include/ucdr/config.h
+++ twist_publisher/include/ucdr/config.h
@@ -1,25 +1,14 @@
-// Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+
+// Set Micro CRD to match ESP32
 
 #ifndef _MICROCDR_CONFIG_H_
 #define _MICROCDR_CONFIG_H_
 
 // Version defines
-#define MICROCDR_VERSION_MAJOR 2
-#define MICROCDR_VERSION_MINOR 0
-#define MICROCDR_VERSION_MICRO 1
-#define MICROCDR_VERSION_STR "2.0.1"
+//#define MICROCDR_VERSION_MAJOR @PROJECT_VERSION_MAJOR@
+//#define MICROCDR_VERSION_MINOR @PROJECT_VERSION_MINOR@
+//#define MICROCDR_VERSION_MICRO @PROJECT_VERSION_PATCH@
+//#define MICROCDR_VERSION_STR "@PROJECT_VERSION@"
 
 // ucdrEndianness defines
 #define UCDR_MACHINE_ENDIANNESS UCDR_LITTLE_ENDIANNESS
